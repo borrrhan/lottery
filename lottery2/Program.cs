25c8be9c2ac77786d44c1c75cbf35fe4a09281fa
m305-24\w401 <abc@gmail.com>
2018-03-22 15:50:03 +0800

    lotttttttttery

---
+++ lottery2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(0, 100));
+            Console.Read();
+        }
+    }
+}
